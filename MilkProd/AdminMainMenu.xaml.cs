9322f2f27827850a5580f2469f3897c204259717
amazinghowell <amazinghowell@gmail.com>
2019-06-08 11:31:04 +0400

    Добавьте файлы проекта.

---
+++ MilkProd/AdminMainMenu.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MilkProd
+{
+    /// <summary>
+    /// Логика взаимодействия для menu.xaml
+    /// </summary>
+    public partial class menu : Window
+    {
+        
+        public menu()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AdminClients menu = new AdminClients();
+            menu.Closed += (asd, dsa) => Show();
+            Hide();
+            menu.Show();
+        }
+    }
+}
---
+++ MilkProd/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MilkProd
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bdmilkprodEntities bd = new bdmilkprodEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(lgnTB.Text) || string.IsNullOrWhiteSpace(pswTB.Password))
+            {
+
+                MessageBox.Show("Введите все данные");
+                return;
+            }
+
+            var AUser = bd.Worker.FirstOrDefault(x => x.login_worker == lgnTB.Text && x.password_worker == pswTB.Password);
+            if (AUser != null)
+            {
+                menu menu = new menu();
+                menu.Closed += (asd, dsa) => Show();
+                Hide();
+                menu.Show();
+            }
+            else
+                MessageBox.Show("Неверные данные");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
